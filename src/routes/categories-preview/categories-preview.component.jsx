61a88948a65ec9355a8feb99ded54803ab6eedb5
IlyIonut <iliesionut3@gmail.com>
2023-06-03 00:21:48 +0300

    alal

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,22 @@
+import { useContext, Fragment } from 'react';
+import CategoryPreview from '../../components/category-preview/category-preview.compinent';
+import {CategoriesContext} from '../../context/categories.context';
+
+const CategoriesPreview = () =>{
+    const {categoriesMap} = useContext(CategoriesContext);
+    return(
+        <Fragment>
+            {
+                Object.keys(categoriesMap).map((title) => {
+                    const products = categoriesMap[title];
+                    return (
+                        <CategoryPreview key={title} title={title} products = {products} />
+                        );
+                })
+            }
+            
+        </Fragment>
+    );
+}
+
+export default CategoriesPreview;
